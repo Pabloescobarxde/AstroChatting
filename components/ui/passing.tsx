87168d54dd85484d663ac3420e207d2b742fed0c
Pabloescobarxde <Pabloescobar20peto@gmail.com>
2026-04-15 18:53:50 +0100

    Update passing.tsx

--- components/ui/passing.tsx
+++ components/ui/passing.tsx
@@ -4,5 +4,5 @@
   "last_update": "2026-04-14",
   "url": "https://raw.githubusercontent.com/Pabloescobarxde/AstroChatting/main/components/ui/freeze.ranger",
   "Diddy": "iL+gK8N2/L9XpY/B5Z0V1A2B3C4D5E6F7G8H9I0J1K2L3M4N5O6P7Q8R9S0T1U2V3W4X5Y6Z7A8B9C0D1E2F3G4H5I6J7K8L9M0N1O2P3Q4R5S6T7U8V9W0X1Y2Z3A4B5C6D7E8F9G0H1I2J3K4L5M6N7O8P9Q0R1S2T3U4V5W6X7Y8Z",
-  "main": "https://cdn.discordapp.com/attachments/1494028942665977946/1494029203761397985/ranger.jar?ex=69e11e9f&is=69dfcd1f&hm=60bb65707366ea647b6d43c2e1e7ee5561aefa891b511ce87fa16186a5a86ad3&"
+  "main": "https://cdn.discordapp.com/attachments/1494028942665977946/1494032897282408469/RangerV0.jar?ex=69e12210&is=69dfd090&hm=b9a35af8288e1b5ffd9c2c6ac5b7e07e1026f09acbdfece1959a78793e9ca422&"
 }

